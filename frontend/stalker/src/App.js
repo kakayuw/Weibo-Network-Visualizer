c8465c14aba826bbee8dd011388c71fae788e23c
kakayuw <yuhang012@sjtu.edu.cn>
2020-07-11 00:42:50 -0400

    add react framework

---
+++ frontend/stalker/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PlatLayout from './components/layout/layout';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PlatLayout/>
+    </div>
+  );
+}
+
+export default App;
+
+
